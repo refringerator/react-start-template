fa97eac74303bd14718a568ec1bd787c09f6da6a
petruha <refloader@gmail.com>
2023-11-04 20:42:04 +0300

    Прекратить опыты  вложенных сторибуков

--- src/components/layout/layout.stories.tsx
+++ src/components/layout/layout.stories.tsx
@@ -3,8 +3,6 @@ import { Layout } from './Layout';
 import { Header } from '../header/Header';
 import { Logo } from '../logo/Logo';
 
-import { HeaderWithLogo } from '../header/header.stories';
-
 const meta: Meta<typeof Layout> = {
   title: 'Общее/Каркас(layout)',
   component: Layout,
