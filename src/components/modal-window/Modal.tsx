ceeeda42db8d510cd5e2bfec2c7ac9d1cf8404ab
petruha <refloader@gmail.com>
2023-11-06 17:03:11 +0300

    Добавить кнопку закрытия в модальное окно

--- src/components/modal-window/Modal.tsx
+++ src/components/modal-window/Modal.tsx
@@ -1,16 +1,19 @@
-import React, { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import './modal.css';
+import { Button } from '../button/Button';
 
 interface ModalProps {
   visible: boolean;
 }
 
-export const Modal: FC<PropsWithChildren<ModalProps>> = ({ visible, children }) => {
-  return (
-    visible && (
-      <div className="modal">
-        <div className="modal--body">{children}</div>
+export const Modal: FC<PropsWithChildren<ModalProps>> = ({ visible, children }) =>
+  visible && (
+    <div className="modal">
+      <div className="modal--body">
+        <div className="modal--body_text-container">{children}</div>
+        <div className="modal--close">
+          <Button type="icon">&#120;</Button>
+        </div>
       </div>
-    )
+    </div>
   );
-};
