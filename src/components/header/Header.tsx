6d1e2914891a4cd3e37f71f8a40124bbd835ddd2
petruha <refloader@gmail.com>
2023-11-09 20:51:58 +0300

    Убрать реакт из импортов

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import './header.css';
 
 export const Header: FC<PropsWithChildren> = ({ children }) => (
--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import './logo.css';
 import LogoSvg from '../../logo.svg';
 
