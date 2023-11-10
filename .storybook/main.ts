a2e007bb5e1d50db4c247a196996560ce2bb9b28
petruha <refloader@gmail.com>
2023-11-10 18:28:34 +0300

    Install storybook/addon-themes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,12 @@
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-themes',
+    '@storybook/themes'
+  ],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,18 @@
 import type { Preview } from '@storybook/react';
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { withThemeFromJSXProvider } from '@storybook/addon-themes';
+
+/* TODO: update import for your custom theme configurations */
+import { lightTheme, darkTheme } from '../path/to/themes';
+
+/* TODO: replace with your own global styles, or remove */
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+  `;
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -15,6 +28,18 @@ const preview: Preview = {
       },
     },
   },
+
+  decorators: [
+    withThemeFromJSXProvider({
+      themes: {
+        light: lightTheme,
+        dark: darkTheme,
+      },
+      defaultTheme: 'light',
+      Provider: ThemeProvider,
+      GlobalStyles,
+    }),
+  ],
 };
 
 export default preview;
