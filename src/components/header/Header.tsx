e5484908a7bcb72e5c551f1e3dffcf8f66d35c57
petruha <refloader@gmail.com>
2023-11-11 23:56:43 +0300

    Добавить в шапку переключатель схемы

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,24 @@
 import { FC, PropsWithChildren } from 'react';
-import './header.css';
+import styled from 'styled-components';
+import theme from 'styled-theming';
+
+const headerBackgroundColor = theme('mode', {
+  ocean: 'rgb(48, 96, 138)',
+  forest: '#182612',
+});
+
+const StyledHeader = styled.div`
+  position: sticky;
+  top: 0;
+  padding: 1em;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${headerBackgroundColor};
+`;
 
 export const Header: FC<PropsWithChildren> = ({ children }) => (
-  <div className="header">
+  <StyledHeader>
     <div className="header--inner">{children}</div>
-  </div>
+  </StyledHeader>
 );
