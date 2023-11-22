1b0799142d655435fef29c540f276bc2d264042f
petruha <refloader@gmail.com>
2023-11-22 18:02:25 +0300

    Добавить переключатель тем в приложение

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
 import './App.css';
 import { Layout } from './components/layout/Layout';
 import { Header } from './components/header/Header';
-import { Logo } from './components/logo/Logo';
 import { Switch } from './components/switch/Switch';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+
+type TTheme = 'ocean' | 'forest';
+const defaultTheme: TTheme = 'ocean';
 
 function App() {
+  const [themeName, setTheme] = useState(defaultTheme);
+
+  const HandleThemeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const theme = e.target.checked ? 'forest' : 'ocean';
+    setTheme(theme);
+  };
+
   return (
     <div className="App">
-      <Layout
-        header={
-          <Header>
-            <Logo />
-            <Switch />
-          </Header>
-        }
-      >
-        <p>Я пример приложения</p>
-      </Layout>
+      <ThemeProvider theme={{ mode: themeName || 'ocean' }}>
+        <Layout
+          header={
+            <Header>
+              <Switch onChange={HandleThemeChange} switched={themeName !== defaultTheme} />
+              <Switch />
+            </Header>
+          }
+        >
+          <p>Я пример приложения</p>
+        </Layout>
+      </ThemeProvider>
     </div>
   );
 }
